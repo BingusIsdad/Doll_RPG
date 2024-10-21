3895c41c7c05c6e85c1a1b0d031f193eb2373b8f
arist4523 <ARist4523@student.pps.net>
2024-10-21 09:38:08 -0700

    Updated Tiling System

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -9,10 +9,10 @@ public class GamePanel extends JPanel implements Runnable {
     final int originalTileSize = 16; //16x16 babyyyy
     final int scale = 3;
     public final int tileSize = originalTileSize * scale; //48 x 48 tile size
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
-    final int screenWidth = tileSize * maxScreenCol;  //768 px
-    final int screenHeight = tileSize * maxScreenRow; //576 px
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
+    public final int screenWidth = tileSize * maxScreenCol;  //768 px
+    public final int screenHeight = tileSize * maxScreenRow; //576 px
     //FPS
     int FPS = 60;
     TileManager tileM = new TileManager(this);
--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -17,17 +17,29 @@ public class TileManager {
 try{
     tile[0] = new Tile();
     tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/WALll.png"));
-   // tile[1] = new Tile();
-   // tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/WALll.png"));
-    //tile[2] = new Tile();
-    //tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/WALll.png"));
+   tile[1] = new Tile();
+    tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/Placeholder_Grass.png"));
+    tile[2] = new Tile();
+    tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/tomawa.png"));
 }catch(IOException e){
     e.printStackTrace();
 }
     }
     public void draw(Graphics2D g2){
-        g2.drawImage(tile[0].image, 0,0,gp.tileSize,gp.tileSize,null);
-        //g2.drawImage(tile[1].image, 48,0,gp.tileSize,gp.tileSize,null);
-        //g2.drawImage(tile[2].image, 9,0,gp.tileSize,gp.tileSize,null);
+int col = 0;
+int row = 0;
+int x = 0;
+int y = 0;
+while(col < gp.maxScreenCol && row < gp.maxScreenRow){
+    g2.drawImage(tile[1].image,x, y,gp.tileSize,gp.tileSize,null);
+    col++;
+    x+= gp.tileSize;
+    if(col == gp.maxScreenCol){
+        col = 0;
+        x = 0;
+        row ++;
+        y += gp.tileSize;
+    }
+}
     }
 }
